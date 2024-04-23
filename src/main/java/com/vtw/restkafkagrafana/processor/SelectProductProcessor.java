3977340a3a76c6617611616d303be0736debaaeb
배지현 <jihyeon.bae@vtw.co.kr>
2024-04-23 17:05:50 +0900

    Service Modified.

--- src/main/java/com/vtw/restkafkagrafana/processor/SelectProductProcessor.java
+++ src/main/java/com/vtw/restkafkagrafana/processor/SelectProductProcessor.java
@@ -4,19 +4,15 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.springframework.stereotype.Component;
 
-import java.time.Instant;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Random;
-
 @Component("SelectProduct")
 public class SelectProductProcessor implements Processor {
 
     @Override
     public void process(Exchange exchange) throws Exception {
-        String productType = (String) exchange.getIn().getHeader("productType");
-        String product = (String) exchange.getIn().getHeader("product");
+        // carInspect forest srtTicket
+        String productType = "srtTicket";
         exchange.setProperty("productType", productType);
+        String product = (String) exchange.getIn().getHeader("product");
         exchange.setProperty("product", product);
 
         if (product == null || product.equals("")) {
@@ -26,7 +22,7 @@ public class SelectProductProcessor implements Processor {
             exchange.getIn().setBody("[선택 없음 오류] 상품이 선택되지 않았습니다.");
             throw new RuntimeException("NotExistChoice Error");
         }
-        if (!product.equals("TV") && !product.equals("T-shirt")) {
+        if (!product.equals("ticketA")) {
             exchange.setProperty("fail", "true");
             exchange.setProperty("errorType", "NullPointerException");
             exchange.setProperty("errorMessage", "[상품 조회 오류] 존재하지 않는 상품입니다.");
